63f83e4b6492c65cefc718ed3f3406dcea8e9be3
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-08-10 23:24:54 -0300

    Project created

---
+++ src/main/java/com/springboot/sitederifa/SitederifaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sitederifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitederifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitederifaApplication.class, args);
+	}
+
+}
