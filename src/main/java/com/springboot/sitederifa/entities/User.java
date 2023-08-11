43a38ec3804eba9339032a0dc3efc5a52ce14ca8
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-08-11 00:04:10 -0300

    H2 database, test profile

--- src/main/java/com/springboot/sitederifa/entities/User.java
+++ src/main/java/com/springboot/sitederifa/entities/User.java
@@ -1,9 +1,21 @@
 package com.springboot.sitederifa.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String phone;
