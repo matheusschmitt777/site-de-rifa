630fc1883ca8bd3bfefb3e3f3d3cabda7c76346b
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-09-06 20:41:55 -0300

    Client insert, delete and update - Exception handling (findById, delete and update)

--- src/main/java/com/springboot/sitederifa/resources/ClientResource.java
+++ src/main/java/com/springboot/sitederifa/resources/ClientResource.java
@@ -1,13 +1,19 @@
 package com.springboot.sitederifa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.sitederifa.entities.Client;
 import com.springboot.sitederifa.services.ClientService;
@@ -17,17 +23,36 @@ import com.springboot.sitederifa.services.ClientService;
 public class ClientResource {
 	
 	@Autowired
-	private ClientService userService;
+	private ClientService clientService;
 	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = userService.findAll();
+		List<Client> list = clientService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Client> findById(@PathVariable Long id) {
-		Client obj = userService.findById(id);
+		Client obj = clientService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Client> insert(@RequestBody Client obj) {
+		obj = clientService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		clientService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client obj) {
+		obj = clientService.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/springboot/sitederifa/services/ClientService.java
+++ src/main/java/com/springboot/sitederifa/services/ClientService.java
@@ -1,26 +1,68 @@
 package com.springboot.sitederifa.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.springboot.sitederifa.entities.Client;
 import com.springboot.sitederifa.repositories.ClientRepository;
+import com.springboot.sitederifa.services.exceptions.DatabaseException;
+import com.springboot.sitederifa.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ClientService {
 	
 	@Autowired
-	private ClientRepository userRepository;
+	private ClientRepository clientRepository;
 	
 	public List<Client> findAll() {
-		return userRepository.findAll();
+		return clientRepository.findAll();
 	}
 	
 	public Client findById(Long id) {
-		Optional<Client> obj = userRepository.findById(id);
+		Optional<Client> obj = clientRepository.findById(id);
 		return obj.get();
+	}
+	@Transactional
+	public Client insert(Client obj) {
+	    obj.setMomentCreated(Instant.now());
+	    return clientRepository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			if (clientRepository.existsById(id)) {
+				clientRepository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException(id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	@Transactional
+	public Client update(Long id, Client obj) {
+		try {
+			Client entity = clientRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return clientRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public void updateData(Client entity, Client obj) {
+		entity.setName(obj.getName());
+		entity.setPhone(obj.getPhone());
+		entity.setFile(obj.getFile());
+		entity.setClientStatus(obj.getClientStatus());
 	}
 }
